15c2b0e126a9c18f479e13a33cccd1d3456fa141
hkayy33 <hkayy33@gmail.com>
2025-08-14 10:43:57 +0100

    TDD: Created a list that stores 3 characters

---
+++ dndApp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+
+    
+    static void Main(string[] args)
+    {
+        //Teams.TeamChoice();
+    }
+}
+
+public class Teams
+{
+    private static List<string> Team = new List<string>();
+    public static int TeamChoice()
+    {
+        Team.Add("Fighter");
+        Team.Add("Wizard");
+        Team.Add("Cleric");
+
+        int number = Team.Count();
+
+        return number;
+    }
+
+}
---
+++ dndAppTester/Tests/GameFunctionTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+
+public class GameFunctionTests
+{
+    // A list of team players 
+    private Program main;
+
+    [SetUp]
+    public void setup()
+    {
+        main = new Program();
+    }
+    [Test]
+    public void Check_If_Three_Characters_Are_Stored_In_A_List()
+    {
+        Assert.That(Teams.TeamChoice, Is.EqualTo(3));
+    }
+}
